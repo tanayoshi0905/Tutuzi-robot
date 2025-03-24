8385d194767ebb3cd7e8e424a586337ccf8cecf8
田邉佳之 <yossy@tana-yossy-mac.local>
2025-03-24 13:42:54 +0900

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+const int limit[] = {}; //ステアリング右、左、矢押し出しギア、押し出し末端
+const int pwm[] = {}; //ステアリング旋回、タイヤ右、左、矢射出上、下
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
